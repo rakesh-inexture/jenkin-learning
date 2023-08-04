8ea3aeeb76fa294a32b6b307552969d71c98e413
shubhamsinha1010 <sinhashubham1911@gmail.com>
2023-08-04 22:24:17 +0530

    fix:removed files

--- main.py
+++
@@ -1,8 +0,0 @@
-def add_num(x,y):
-  return x+y
-
-def sub_num(x,y):
-  return x-y
-
-def mul_num(x,y):
-  return x*y
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,8 +1,8 @@
-from main import *
+from content.main import add_num, sub_num
 
 
 def test_add():
-  assert add_num(4,5)==10
+  assert add_num(4,5)==9
 
 def test_sub():
   assert sub_num(6,4)==2
