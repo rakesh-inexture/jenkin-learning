9f3687eb94963f4d51566e867c092dd383e2685f
shubhamsinha1010 <sinhashubham1911@gmail.com>
2023-08-14 21:47:49 +0530

    fix:Added major modifications by adding fastapi basic project

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def add_num(x,y):
-  return x+y
-  
-def sub_num(x,y):
-  return x-y
+from fastapi import FastAPI, Request
+import uvicorn
 
-def mul_num(x,y):
-  return x*y
+app = FastAPI()
 
 
+@app.get("/")
+async def signin(request:Request):
+    return {"message": "HELLO WORLD"}
 
 
+if __name__ == '__main__':
+        uvicorn.run(
+        "app:app",
+        host    = "0.0.0.0",
+        port    = 8037,
+        reload  = True
+    )
